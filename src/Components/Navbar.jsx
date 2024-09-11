57e284602362ff4b15905959210c7c57d8544ed4
afzal-2004 <moa44468@gmail.com>
2024-09-12 01:52:22 +0530

    Adding Css To modify

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,9 @@ export const Navbar = () => {
   const [openMenu, setopenMenu] = useState(false);
   return (
     <>
-      <div className=" NavItems ">
+      <div className="  absolute NavItems">
         <NavItems />
+
         <TiThMenu
           className=" sm:hidden z-50"
           onClick={() => {
@@ -26,80 +27,82 @@ const NavItems = ({ mobile, setopenMenu }) => {
   return (
     <>
       <h1>Afzal</h1>
-      <ul
-        className={`flex ${mobile ? "flex-col" : "gap-4"} cursor-pointer ${
-          mobile ? "MobileItems" : "sm:flex hidden"
-        } ${`setopenMenu?"navItemsSmooth": " "`}`}
-      >
-        {" "}
-        <li className="sm:hidden   w-full  flex justify-end  m-[3vh] p-3">
-          <RxCross1
-            onClick={() => {
-              setopenMenu(false);
-            }}
-          />
-        </li>
-        <li>
-          <a
-            href="#Home"
-            onClick={() => {
-              setopenMenu(false);
-            }}
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#Skills"
-            onClick={() => {
-              setopenMenu(false);
-            }}
-          >
-            Skills
-          </a>
-        </li>
-        <li>
-          <a
-            href="#Projects"
-            onClick={() => {
-              setopenMenu(false);
-            }}
-          >
-            Projects
-          </a>
-        </li>
-        <li>
-          <a
-            href="#Experience"
-            onClick={() => {
-              setopenMenu(false);
-            }}
-          >
-            Experience
-          </a>
-        </li>
-        <li>
-          <a
-            href="#Service"
-            onClick={() => {
-              setopenMenu(false);
-            }}
-          >
-            Service
-          </a>
-        </li>
-        <li>
-          <a
-            href="#Conatct"
-            onClick={() => {
-              setopenMenu(false);
-            }}
-          >
-            Conatct
-          </a>
-        </li>
-      </ul>
+      <div className="   ">
+        <ul
+          className={`flex ${mobile ? "flex-col" : "gap-4"} cursor-pointer ${
+            mobile ? "MobileItems" : "sm:flex hidden"
+          } ${`setopenMenu?"navItemsSmooth": " "`}`}
+        >
+          {" "}
+          <li className="sm:hidden   w-full  flex justify-end  m-[3vh] p-3">
+            <RxCross1
+              onClick={() => {
+                setopenMenu(false);
+              }}
+            />
+          </li>
+          <li>
+            <a
+              href="#Home"
+              onClick={() => {
+                setopenMenu(false);
+              }}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#Skills"
+              onClick={() => {
+                setopenMenu(false);
+              }}
+            >
+              Skills
+            </a>
+          </li>
+          <li>
+            <a
+              href="#Projects"
+              onClick={() => {
+                setopenMenu(false);
+              }}
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="#Experience"
+              onClick={() => {
+                setopenMenu(false);
+              }}
+            >
+              Experience
+            </a>
+          </li>
+          <li>
+            <a
+              href="#Service"
+              onClick={() => {
+                setopenMenu(false);
+              }}
+            >
+              Service
+            </a>
+          </li>
+          <li>
+            <a
+              href="#Conatct"
+              onClick={() => {
+                setopenMenu(false);
+              }}
+            >
+              Conatct
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
